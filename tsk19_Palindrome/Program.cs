f7eca69dc2351927ff02eba8f7cf5479333e1707
Shoshela <goyati@mail.ru>
2022-11-25 14:51:26 +0300

    Add tsk19_Palindrome

---
+++ tsk19_Palindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.Write("Enter namber...  ");
+string number = Console.ReadLine();
+
+if (number.Length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} is palindrome");
+    }
+    else
+    {
+        Console.WriteLine($"{number} is not a palindrome");
+    }
+}
+else
+{
+    Console.WriteLine($"{number} is not five-digit");
+}
